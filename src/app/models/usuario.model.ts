c77492ca4516bdfbc3556b6a66e63ade625ae52c
Maria Gabriela S. A <mgabrielaprofissional@hotmail.com>
2023-05-24 17:11:47 -0300

    mudanças

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,13 @@
+export class Usuario {
+    id?:any;
+    nome?: string;
+    email?: string;
+    senha?: string
+    token?: string;
+    is_admin?: boolean;
+
+    constructor(email: string, senha: string) {
+        this.email;
+        this.senha;
+    }
+}
